88fa9d1302d74ad65951c38aa6d8e9b045c27845
EloiAlsina <eloi.alsina.7e2@itb.cat>
2022-05-16 19:11:25 +0200

    Seguim amb autenticació

---
+++ src/main/java/cat/itb/jugadorsspring/model/repositoris/RepositoriUsuari.java
@@ -0,0 +1,12 @@
+package cat.itb.jugadorsspring.model.repositoris;
+
+import cat.itb.jugadorsspring.model.entitats.Usuari;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RepositoriUsuari extends JpaRepository<Usuari, Long> {
+    Optional<Usuari> findByUsername(String username);
+}
---
+++ src/main/java/cat/itb/jugadorsspring/model/serveis/ServeiUsuari.java
@@ -0,0 +1,35 @@
+package cat.itb.jugadorsspring.model.serveis;
+
+import cat.itb.jugadorsspring.model.entitats.Usuari;
+import cat.itb.jugadorsspring.model.repositoris.RepositoriUsuari;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ServeiUsuari {
+
+    private final RepositoriUsuari repositoriUsuari;
+
+    private final PasswordEncoder xifrat;
+
+    public Usuari consultarPerUsername(String username) {
+        return repositoriUsuari.findByUsername(username).orElse(null);
+    }
+
+    public Usuari crearNouUsuari(Usuari nouUsuari) {
+        //falta controlar que els 2 passwords del client coincideixen
+        //passar un UsuariCreacioDTO
+        nouUsuari.setPassword(xifrat.encode(nouUsuari.getPassword()));
+        repositoriUsuari.save(nouUsuari);
+        return nouUsuari;
+    }
+
+    public List<Usuari> llistarUsuaris(){
+        return repositoriUsuari.findAll();
+    }
+
+}
